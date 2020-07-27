44c06ab1e19dad87ac048b2bc9044c9525c77dfb
Alessandro <alex.ixs@hotmail.com>
2020-07-27 13:11:49 +0200

    start

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php include __DIR__ . "/database.php"; ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Boolean Hotel</title>
+     <link rel="stylesheet" href="dist/app.css">
+   </head>
+   <body>
+    <div class="wrapper">
+      <!-- Begin header -->
+      <header>
+        <h1>Hotel da incubo</h1>
+      </header>
+      <!-- End Header -->
+
+      <!-- Begin main -->
+      <main>
+        <!-- Begin room list table -->
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>N Stanza</th>
+              <th>Piano</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($rooms as $room) { ?>
+              <tr>
+                <td> <?php echo $room['id']; ?> </td>
+                <td> <?php echo $room["room_number"]; ?> </td>
+                <td> <?php echo $room["floor"]; ?> </td>
+                <td> <a href="http://localhost:8888/esercizio-boolean-hotel/room_details.php?id=<?php echo $room['id'];?>">View room details</a> </td>
+              </tr>
+            <?php }; ?>
+
+          </tbody>
+        </table>
+        <!-- End room list table -->
+       </main>
+       <!-- End main -->
+     </div>
+   </body>
+ </html>
